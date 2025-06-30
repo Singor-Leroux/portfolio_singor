ca75ce4837a98245680baab292af3fe00d8b2912
Singor.KPATCHOU <skpatchou@hyperion-dev.com>
2025-06-30 16:04:52 +0200

    Correction de la configuration TypeScript et des dépendances

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,9 +1,16 @@
 import { IUser } from '../models/user.model';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
 
 declare global {
   namespace Express {
     interface Request {
       user?: IUser;
+      file?: any; // Pour les fichiers téléchargés
+    }
+
+    interface MulterRequest extends Request {
+      file?: Express.Multer.File;
+      files?: { [fieldname: string]: Express.Multer.File[] } | Express.Multer.File[];
     }
   }
 }
@@ -11,3 +18,11 @@ declare global {
 export interface IRequestWithUser extends Request {
   user: IUser;
 }
+
+// Déclaration des modules manquants
+declare module 'jsonwebtoken';
+declare module 'multer';
+declare module 'nodemailer';
+declare module 'cookie-parser';
+
+export {};
